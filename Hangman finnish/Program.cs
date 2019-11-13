af5bf35d2d78af276e22d84b1d80d40b00de7220
Hasmukh Rathod <guru__@hotmail.com>
2019-11-13 16:21:43 +0100

    Add project files.

---
+++ Hangman finnish/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hangman_finnish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            string[] wordBank = { " blomma", "stad", "gul", "växjö" };
+            string GameWords = wordBank[random.Next(wordBank.Count() -1)];
+            string GameWordsUpercase = GameWords.ToUpper();
+
+            Console.WriteLine(GameWordsUpercase);
+            StringBuilder dispToPlayer = new StringBuilder(GameWords.Length);
+
+            for (int i = 0; i < wordBank.Length; i++)
+                dispToPlayer.Append('_');
+            //Console.WriteLine($" {wordBank});
+           
+            
+        }
+    }
+}
